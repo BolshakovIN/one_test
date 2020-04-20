db651a77fbbcbee8e2ad124edac43380f530929f
Иван Большаков <ivanbolsakov@MacBook-Air-Ivan.local>
2020-04-20 19:36:24 +0300

    test

---
+++ lesson_1.6.11_recens.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+import time
+
+try:
+    link = "http://suninjuly.github.io/registration2.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element_by_css_selector('.first_class .first:nth-child(2)[placeholder="Input your first name"]')
+    input1.send_keys("Ivan")
+    input2 = browser.find_element_by_css_selector('.second_class .second:nth-child(2)[placeholder="Input your last name"]')
+    input2.send_keys("Petrov")
+    input3 = browser.find_element_by_css_selector('.third[placeholder="Input your email"]')
+    input3.send_keys("lala@mail.ru")
+    button = browser.find_element_by_css_selector("button.btn")
+    button.click()
+
+    time.sleep(1)
+
+    welcome_text_elt = browser.find_element_by_tag_name("h1")
+    welcome_text = welcome_text_elt.text
+
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    time.sleep(10)
+    browser.quit()
